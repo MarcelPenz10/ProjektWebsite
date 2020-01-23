6420f4949ed8f943a3910f092349ca0f3f8bb502
Ghosti31 <tofloeckinger@tsn.at>
2020-01-23 15:36:07 +0100

    park

--- Projekt/Projekt/Scripts/JQuery.js
+++ Projekt/Projekt/Scripts/JQuery.js
@@ -1,7 +1,9 @@
 ﻿$(document).ready(function(){
     $(".ParkImg").hover( function(){
         $(this).find('p').show().offset($(this).offset());
+        $(this).find('img').css('opacity', '0.6');
     }, function(){
         $(this).find('p').hide();
+        $(this).find('img').css('opacity', '1');
     });
 });
