c2fc2576898a1550a47313903784d37516259430
Ghosti31 <tofloeckinger@tsn.at>
2020-01-29 10:33:56 +0100

    DB

--- Projekt/Projekt/Controllers/ParkController.cs
+++ Projekt/Projekt/Controllers/ParkController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Projekt.Models.DB;
 
 namespace Projekt.Controllers
 {
     public class ParkController : Controller
     {
+        IUser user = new UserDB(); 
+
         public ActionResult Index()
         {
             return View ();
