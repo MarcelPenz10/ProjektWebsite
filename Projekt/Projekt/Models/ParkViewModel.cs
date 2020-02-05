3bd7d7be278689a557978e7395ddc008ee2d5c87
Ghosti31 <tofloeckinger@tsn.at>
2020-02-05 10:28:55 +0100

    Img

---
+++ Projekt/Projekt/Models/ParkViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Projekt.Models;
+using System.IO;
+
+namespace Projekt.Models
+{
+    public class ParkViewModel
+    {
+        public Park Park { get; set; }
+        public DirectoryInfo DirectoryInfo { get; set; }
+        public FileInfo[] FileInfo { get; set; }
+
+        public ParkViewModel() : this(new Park(), null, null) { }
+        public ParkViewModel(Park park, DirectoryInfo directoryInfo, FileInfo[] fileInfo)
+        {
+            this.Park = park;
+            this.DirectoryInfo = directoryInfo;
+            this.FileInfo = fileInfo;
+        }
+    }
+}
